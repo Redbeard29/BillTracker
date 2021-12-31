ecf505da0c03ad44eccd5159e1159a25e83dbd45
Redbeard29 <ben@benshermanmusic.com>
2021-12-31 10:00:18 -0500

    added logic to verify that adjustments made matches amount of entered difference

--- BillTracker.py
+++ BillTracker.py
@@ -53,6 +53,76 @@ def valid_num_times(times):
     else:
         return True
 
+def add_adjustments():
+    
+    num_of_trans = input()
+    while not valid_num_times(num_of_trans):
+        num_of_trans = input("Invalid entry. You must enter a whole number.\n")
+
+    num_of_trans = int(num_of_trans)
+    sum_of_trans = 0
+
+    for trans in range(num_of_trans):
+        reason = input(f"Transaction {trans + 1}:\n")    
+        cost = input("How much?\n")
+        #account for user entering $num instead of just num
+        cost = cost.replace("$", "")
+
+        while not is_valid_input(cost):
+            cost = input("Invalid entry. You must enter a number.\n")
+
+        cost = round(float(cost), 2)
+        print(f"Entered ${cost:.2f} for {reason}")
+        
+        sum_of_trans += cost
+
+        create_note = input("Enter associated notes? Y/N\n").upper()
+        
+        while create_note not in acceptable_answers:
+            create_note = input("Invalid entry. Please enter Y or N.\n").upper()
+
+        if create_note == acceptable_answers[1]:
+            note = input(f"Notes for ${cost:.2f} {reason}:\n")
+            print(f'"{note}" stored as notes for {reason}')
+    
+    return sum_of_trans, num_of_trans
+
+def subtract_adjustments():
+    
+    num_of_trans = input()
+    while not valid_num_times(num_of_trans):
+        num_of_trans = input("Invalid entry. You must enter a whole number.\n")
+
+    num_of_trans = int(num_of_trans)
+    sum_of_trans = 0
+
+    for trans in range(num_of_trans):
+
+        reason = input(f"Transaction {trans + 1}:\n")    
+        cost = input("How much?\n")
+        #account for user entering $num instead of just num
+        cost = cost.replace("$", "")
+
+        while not is_valid_input(cost):
+            cost = input("Invalid entry. You must enter a number.\n")
+
+        cost = round(float(cost), 2)
+
+        print(f"Subtracted ${cost:.2f} from total for {reason}")
+
+        sum_of_trans += cost
+
+        create_note = input("Enter associated notes? Y/N\n").upper()
+        
+        while(create_note not in acceptable_answers):
+            create_note = input("Invalid entry. Please enter Y or N.\n").upper()
+
+        if create_note == acceptable_answers[1]:
+            note = input(f"Notes for ${cost:.2f} {reason}:\n")
+            print(f'"{note}" stored as notes for {reason}')
+
+    return sum_of_trans, num_of_trans
+
 total_entered = input("How much did you transfer for this two week period?:\n")
 
 #account for user entering $num instead of just num
@@ -66,85 +136,31 @@ acceptable_answers = ["N", "Y"]
 
 if not total_entered == total_biweekly_entry:
 
-    sum_of_trans = 0 
-
     if total_entered > total_biweekly_entry:
-        
         difference = total_entered - total_biweekly_entry
-
         print(f"You entered ${total_entered:.2f}, which is ${difference:.2f} more than expected. How many extra transactions are you accounting for?")
-        
-        num_of_trans = input()
-        while not valid_num_times(num_of_trans):
-            num_of_trans = input("Invalid entry. You must enter a whole number.\n")
+        sum_of_trans, num_of_trans = add_adjustments()
 
-        num_of_trans = int(num_of_trans)
+        while sum_of_trans < difference:
+            trans_difference = difference - sum_of_trans
+            print(f"You entered ${sum_of_trans:.2f}, which is ${trans_difference:.2f} short of ${difference:.2f}. How many more transactions would you like to add to account for this difference?")
+            sum_of_extra_trans, num_of_extra_trans = add_adjustments()
+            sum_of_trans += sum_of_extra_trans
+            num_of_trans += num_of_extra_trans
 
-        for trans in range(num_of_trans):
-            reason = input(f"Transaction {trans + 1}:\n")    
-
-            cost = input("How much?\n")
-
-            #account for user entering $num instead of just num
-            cost = cost.replace("$", "")
-
-            while not is_valid_input(cost):
-                cost = input("Invalid entry. You must enter a number.\n")
-
-            cost = round(float(cost), 2)
-            print(f"Entered ${cost:.2f} for {reason}")
-            
-            sum_of_trans += cost
-
-            create_note = input("Enter associated notes? Y/N\n").upper()
-            
-            while create_note not in acceptable_answers:
-                create_note = input("Invalid entry. Please enter Y or N.\n").upper()
-
-            if create_note == acceptable_answers[1]:
-                note = input(f"Notes for ${cost:.2f} {reason}:\n")
-                print(f'"{note}" stored as notes for {reason}')
+        while sum_of_trans > difference:
+            trans_difference = sum_of_trans - difference
+            print(f"You entered ${sum_of_trans:.2f}, which is ${trans_difference:.2f} too much. Please re-enter adjustments in an amount totalling ${difference:.2f}. How many transactions are you accounting for?")
+            sum_of_trans, num_of_trans = add_adjustments()
 
     else:
-
         difference = total_biweekly_entry - total_entered
-
         print(f"You entered ${total_entered:.2f}, which is ${difference:.2f} less than expected. How many transactions do you want to identify as not needed?")
-        
-        num_of_trans = input()
-        while not valid_num_times(num_of_trans):
-            num_of_trans = input("Invalid entry. You must enter a whole number.\n")
+        sum_of_trans, num_of_trans = add_adjustments()
 
-        num_of_trans = int(num_of_trans)
-
-        for trans in range(num_of_trans):
-
-            reason = input(f"Transaction {trans + 1}:\n")    
-
-            cost = input("How much?\n")
-
-            #account for user entering $num instead of just num
-            cost = cost.replace("$", "")
-
-            while not is_valid_input(cost):
-                cost = input("Invalid entry. You must enter a number.\n")
-
-            cost = round(float(cost), 2)
-
-            print(f"Subtracted ${cost:.2f} from total for {reason}")
-
-            sum_of_trans += cost
-
-            create_note = input("Enter associated notes? Y/N\n").upper()
-            
-            while(create_note not in acceptable_answers):
-                create_note = input("Invalid entry. Please enter Y or N.\n").upper()
-
-            if create_note == acceptable_answers[1]:
-                note = input(f"Notes for ${cost:.2f} {reason}:\n")
-                print(f'"{note}" stored as notes for {reason}')
-               
     print(f"Sum of {num_of_trans} extra transactions: ${sum_of_trans:.2f}")
+
+               
 
 else:
     print(f"You entered ${total_entered:.2f}, which is exactly what was expected for this period.")
